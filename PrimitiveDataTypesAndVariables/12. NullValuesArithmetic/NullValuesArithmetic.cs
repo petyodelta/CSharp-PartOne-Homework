7a8249815115fc8a66028ecde97e60d704b05d2a
petyodelta <petyodelta@gmail.com>
2015-01-23 16:10:38 +0200

    12. NullValuesArithmetic change

--- PrimitiveDataTypesAndVariables/12. NullValuesArithmetic/NullValuesArithmetic.cs
+++ PrimitiveDataTypesAndVariables/12. NullValuesArithmetic/NullValuesArithmetic.cs
@@ -6,10 +6,10 @@ class NullValuesArithmetic
     {
         int? firstNum = null;
         double? secondNum = null;
-        Console.WriteLine("First number is !{0}!", firstNum);
-        Console.WriteLine("Second number is !{0}! ", secondNum);
-        firstNum = 5;
-        secondNum = 123.456;
+        Console.WriteLine("First number is {0}", firstNum);
+        Console.WriteLine("Second number is {0}", secondNum);
+        firstNum++;
+        secondNum++;
         Console.WriteLine("First number is " + firstNum);
         Console.WriteLine("Second number is " + secondNum);
     }
